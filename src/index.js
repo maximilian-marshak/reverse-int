b8b508c01a316a31f32fdef63766e0f31d2d78ed
Maximilian Marshak <maximilian.marshak@gmail.com>
2023-02-28 01:17:06 +0300

    first

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    const reversed = n.toString().split('').reverse().join('');
+    return Math.sign(n) * parseInt(reversed);
 }
+
+console.log(reverse(123))
